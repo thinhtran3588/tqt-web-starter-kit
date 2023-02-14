31794f5c70ab7c047d73817296eca8f40b71274f
Thinh Tran <quangthinhtran3588@gmail.com>
2023-02-14 22:07:57 +0700

    fix Nextjs cache (#3)

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  headers: async () => [
+      {
+          // list more extensions here if needed; these are all the resources in the `public` folder including the subfolders
+          source: '/assets/:all',
+          locale: false,
+          headers: [
+              {
+                  key: 'Cache-Control',
+                  value: 'public, max-age=86400, stale-while-revalidate',
+              },
+          ],
+      },
+  ],
 }
 
 module.exports = nextConfig
