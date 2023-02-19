e98f5cb90a866facc5ef9dc297cb0da528761b2a
Thinh Tran <quangthinhtran3588@gmail.com>
2023-02-19 21:09:33 +0700

    refactor Nextjs, add next-seo

---
+++ src/common/components/seo.tsx
@@ -0,0 +1,19 @@
+import { NextSeo } from "next-seo";
+
+interface Props {
+  title?: string;
+  description?: string;
+}
+
+export default function SEO({ title, description }: Props) {
+  return (
+    <NextSeo
+      title={
+        title
+          ? `${title} | ${process.env.NEXT_PUBLIC_SITE_NAME}`
+          : process.env.NEXT_PUBLIC_SITE_NAME
+      }
+      description={description || process.env.NEXT_PUBLIC_SITE_DESCRIPTION}
+    />
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import SEO from "@app/common/components/seo";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <SEO />
+      Hello world
+      <div>
+        <Link href="/demo">Demo</Link>
+        <Link href="/auth">Auth</Link>
+        <Link href="/auth/register">Register</Link>
+      </div>
+    </div>
+  );
+}
